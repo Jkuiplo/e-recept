5191f0a9d9dbbdb76ad8c3affd3376ed9cf879ba
grisha <griiisha2111@gmail.com>
2026-04-04 19:15:22 +0500

    Setted up color, theme and type for project

---
+++ app/src/main/java/com/google/e_recept/ui/theme/Color.kt
@@ -0,0 +1,20 @@
+package com.google.e_recept.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val MainBg = Color(0xFFEDEDE9)
+val SecBg = Color(0xFFF5EBE0)
+val MainAc = Color(0xFFD6CCC2)
+val SecAc = Color(0xFFE3D5CA)
+val MainTx = Color(0xFF1F1F1F)
+val SecTx = Color(0xFF525252)
+val FavTx = Color(0xFF20202A)
+val PrimaryPurple = Color(0xFF6B539C)
---
+++ app/src/main/java/com/google/e_recept/ui/theme/Type.kt
@@ -0,0 +1,52 @@
+package com.google.e_recept.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+val Typography = Typography(
+    // Header Bold 64
+    displayLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Bold,
+        fontSize = 64.sp,
+        lineHeight = 72.sp
+    ),
+    // Subheader Bold 32
+    headlineLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Bold,
+        fontSize = 32.sp,
+        lineHeight = 40.sp
+    ),
+    // Title Medium 24
+    titleLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
+        fontSize = 24.sp,
+        lineHeight = 32.sp
+    ),
+    // Subtitle Medium 20
+    titleMedium = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
+        fontSize = 20.sp,
+        lineHeight = 28.sp
+    ),
+    // Regular Medium 16
+    bodyLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp,
+        lineHeight = 24.sp
+    ),
+    // Text Medium 14
+    bodyMedium = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        lineHeight = 20.sp
+    )
+)
