84b0737af238f0b8e456625bc9050e6fe0524cf8
grisha <griiisha2111@gmail.com>
2026-05-02 03:16:06 +0500

    animation faster

--- app/src/main/java/com/google/eRecept/MainScreen.kt
+++ app/src/main/java/com/google/eRecept/MainScreen.kt
@@ -1,5 +1,7 @@
 package com.google.eRecept
 
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -79,6 +81,10 @@ fun MainScreen(
             beyondViewportPageCount = 1,
             flingBehavior = PagerDefaults.flingBehavior(
                 state = pagerState,
+                snapAnimationSpec = spring(
+                    stiffness = Spring.StiffnessMedium,
+                    dampingRatio = Spring.DampingRatioNoBouncy
+                ),
                 snapPositionalThreshold = 0.15f
             )
         ) { page ->
